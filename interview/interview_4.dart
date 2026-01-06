182b04c4acce66069bacb5e5d139555236c627b9
juncook <nguyenngoctuyen188@gmail.com>
2026-01-06 16:11:26 +0700

    #feat:  interview

---
+++ interview/interview_4.dart
@@ -0,0 +1,12 @@
+/// *** VSync (Vertical Synchronization) trong Flutter là cơ chế đồng bộ animation với nhịp làm mới của màn hình,
+///  giúp animation mượt, đúng timing và không tốn tài nguyên.
+/// 
+/// *** what are mixins? Give an example of mixin.
+/// Mixins trong Dart là cơ chế tái sử dụng hành vi cho nhiều class mà không cần kế thừa. 
+/// Chúng cho phép chia sẻ method và field, 
+/// hỗ trợ multiple mixins và tránh được diamond problem bằng cách áp dụng mixin theo thứ tự.
+/// Mixin giải quyết bài toán multiple inheritance (hành vi)
+/// 
+/// *** BuildContext đại diện cho vị trí của một widget trong widget tree.
+///  Nó cho phép widget truy cập các widget tổ tiên như Theme, MediaQuery, Navigator hoặc InheritedWidget.
+/// Việc dùng đúng BuildContext rất quan trọng để tránh lỗi scope và lifecycle trong Flutter.
